1f65254ef82feea75c911c90ce4c542803a3964e
steveli1840 <steveli1840@gmail.com>
2025-11-02 22:24:44 +0800

    my wallet

---
+++ src/checkBalance.js
@@ -0,0 +1,35 @@
+import {ethers } from "ethers";
+import { RPC_URL, LINK_TOKEN } from "./config.js";
+
+async function checkBalance(address) {
+    const provider = new ethers.JsonRpcProvider(RPC_URL);
+
+    // 检查 ETH 余额
+    const ethBalance = await provider.getBalance(address);
+    console.log(`💰地址：${address}`)
+    console.log(`ETH 余额: ${ethers.formatEther(ethBalance)} ETH`);
+
+    // 检查 LINK 余额
+    const erc20 = new ethers.Contract(
+        LINK_TOKEN,
+        [
+            "function balanceOf(address owner) view returns (uint256)",
+            "function decimals() view returns (uint8)"
+        ],
+        provider
+    );
+
+    const decimals = await erc20.decimals();
+    const linkBalance = await erc20.balanceOf(address);
+    console.log(`LINK 余额: ${ethers.formatUnits(linkBalance, decimals)} LINK`);
+}
+
+const address = [
+    "0x38f381819a327b1d96f6b4dc89e5176f129c0dc9",  // My metamask address
+    "0xeDC6c96347F2AEF9c28a2cDFbf4fd05E2A271136",  // My program derived address 0
+    "0x9854F31760cB278a9671af3E4e91540D84224275"   // My program derived address 1
+];
+
+for (const addr of address) {
+    await checkBalance(addr);
+}
--- src/wallet.js
+++ src/wallet.js
@@ -1,7 +1,37 @@
 import { ethers } from "ethers";
-import { RPC_URL, PRIVATE_KEY } from "./config.js";
+import { RPC_URL, PRIVATE_KEY, LINK_TOKEN } from "./config.js";
 import { transferEth } from "./transferEth.js";
 import { transferToken } from "./transferToken.js";
+
+
+/**
+ * 查询并打印一个地址的ETH + LINK余额
+ */
+async function checkBalance(address,provider) {
+  const ethBalance = await provider.getBalance(address);
+
+  const erc20 = new ethers.Contract(
+    LINK_TOKEN,
+    [
+      "function balanceOf(address owner) view returns (uint256)",
+      "function decimals() view returns (uint8)"
+    ],
+    provider
+  );
+
+  let decimals = 18;
+  try {
+    decimals = await erc20.decimals();
+  } catch (error) {
+    console.error("无法获取LINK代币的decimals，使用默认值18");
+  }
+
+  const linkBalance = await erc20.balanceOf(address);
+
+  console.log(`💰 地址：${address}`);
+  console.log(`   ETH 余额: ${ethers.formatEther(ethBalance)} ETH`);
+  console.log(`   LINK 余额: ${ethers.formatUnits(linkBalance, decimals)} LINK`);
+}
 
 async function main() {
   // 1. 生成随机助记词
@@ -10,8 +40,8 @@ async function main() {
 
   // 2. 派生两个地址
   const hdNode = ethers.HDNodeWallet.fromPhrase(mnemonic);
-  const wallet0 = hdNode.derivePath("m/44'/60'/0'/0/0");
-  const wallet1 = hdNode.derivePath("m/44'/60'/0'/0/1");
+  const wallet0 = hdNode.derivePath("44'/60'/0'/0/0");
+  const wallet1 = hdNode.derivePath("44'/60'/0'/0/1");
 
   console.log("地址0:", wallet0.address);
   console.log("地址1:", wallet1.address);
@@ -27,6 +57,12 @@ async function main() {
 
   // 5. 转 LINK
   await transferToken(sender, wallet1.address, "0.1", provider);
+
+  // 6. 查询余额
+  console.log("\n=== 查询余额 ===");
+  await checkBalance(sender.address, provider);
+  await checkBalance(wallet0.address, provider);
+  await checkBalance(wallet1.address, provider);
 }
 
 main().catch(console.error);
